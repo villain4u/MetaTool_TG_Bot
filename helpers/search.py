426d41b013c3524ecdd3629961bdf179d9190199
villain4u <89060402+villain4u@users.noreply.github.com>
2021-08-18 22:36:20 +0530

    Update search.py

--- helpers/search.py
+++ helpers/search.py
@@ -135,8 +135,8 @@ def searchmes(m):
 
                             else:
                                 name = media["name"]
-                                dir_down = "https://{}/view/{}".format(LD_DOMAIN, media["id"])
-                                f_season_html = "<b> - - - - - - - Play/Download In VTube App : </b><a href={}>Click to Play/Download</a> \nLogIn Required! !!<br>".format(dir_down)
+                                dir_down = "intent:https://{}/view/{}#Intent;package={};S.title=@Tube_Movirs {};end".format(LD_DOMAIN, media["id"], APP_INTENT, media["name"])
+                                f_season_html = "<b> - - - - - - - Play/Download In VTube App : </b><a href={}>Click to Play/Download</a> \n<b> - - - - - - - ⚠️Donated Members Only⚠️ </b><br>".format(dir_down)
 
                             TG_html = '''<p>
                                             <img src=''' + str(backdrop) + '''>
