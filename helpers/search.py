2b5f4c7e6c915eec95f19a8321a30d7de468f8c2
villain4u <89060402+villain4u@users.noreply.github.com>
2021-08-18 22:25:54 +0530

    Update search.py

--- helpers/search.py
+++ helpers/search.py
@@ -136,7 +136,7 @@ def searchmes(m):
                             else:
                                 name = media["name"]
                                 dir_down = "https://{}/view/{}".format(LD_DOMAIN, media["id"])
-                                f_season_html = "<b> - - - - - - - Play/Download In VTube App : </b><a href={}>Click to Play/Download</a> \nLogIn Required! !!<br>" : </b><a href={}></a> !!<br>".format(dir_down)
+                                f_season_html = "<b> - - - - - - - Play/Download In VTube App : </b><a href={}>Click to Play/Download</a> \nLogIn Required! !!<br>".format(dir_down)
 
                             TG_html = '''<p>
                                             <img src=''' + str(backdrop) + '''>
